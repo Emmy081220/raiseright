0ceeeeaf77042417bdf92cae7baa867f3a77c7fc
Emmy081220 <emmanuelihezie44@gmail.com>
2023-01-26 16:31:10 +0100

    raiseRight

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+let submitForm = document.querySelector(".submitForm").addEventListener("click", (e) => {
+    e.preventDefault()
+})
+
+let submitBtn = document.querySelector(".submitBtn").addEventListener("click", () => {
+    alert("The BackEnd is still in process")
+})
